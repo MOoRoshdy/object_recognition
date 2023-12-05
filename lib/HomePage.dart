0c91bcdb500c48161b492205534d9b3f0876dc97
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-12-05 13:20:07 +0200

    seatting camers

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,6 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:object_recognition/main.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +10,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isWorking = false;
+  String result = "";
+  CameraController cameraController;
+  CameraImage imgCamera;
+
+  initCamera() {
+    cameraController = cameraController(cameras[0], ResolutionPreset.medium);
+    cameraController.initialize().then((value) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {
+        cameraController.startImageStream((imageFromStream) => {
+          if(!isWorking){
+            isWorking=true,
+            imgCamera=imageFromStream,
+          }
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: SafeArea(child: Scaffold(
+        body: Container(
+          
+        ),)),
+    );
   }
 }
